5f9a22f53b3e6996516f7eaf3ec5323961ccb821
RodResq <rodrigoresque@yahoo.com.br>
2021-12-28 23:27:03 -0300

    frist

---
+++ src/test/java/br/com/treinaweb/sprinbootteste/SprinBootTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.sprinbootteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
